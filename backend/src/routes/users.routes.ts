3aa95fcf5e0fa4c6e3326d9ca09b1fd91b27fc27
Gaïa Durand <florian.durand2@etu.univ-cotedazur.fr>
2026-03-13 11:24:37 +0100

    fix: backend api routes

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,11 +1,35 @@
 import { Response, Router } from "express";
 import pool from "../dbpool.js";
-import { AuthenticatedRequest } from "../types.js";
 import { authMiddleware } from "../middleware/auth.js";
+import { User } from "../types.js";
 
 const router = Router();
 
 // /users/...
+
+// /users/me/groups
+router.get('/me/groups', async (req, res) => {
+    const user : User = (req as any).user;
+
+    let conn;
+    try {
+        conn = await pool.getConnection();
+
+        const groups = await conn.query(
+            `SELECT g.id, g.name 
+            FROM Groups g
+            JOIN GroupUsers gu ON g.id = gu.id 
+            WHERE gu.userId = ?`,
+            [user.id]
+        );
+
+        if (groups.length > 0) {
+            res.json(groups);
+        } else {
+            res.status(404).send("User does not belong to any group");
+        }
+    }
+});
 
 
 
--- backend/src/types.d.ts
+++ backend/src/types.d.ts
@@ -1,7 +1,5 @@
 import { Request } from "express";
 
-export interface AuthenticatedRequest extends Request {
-    user: {
-        id: number;
-    }
-}
+export type User = {
+    id: number
+};
