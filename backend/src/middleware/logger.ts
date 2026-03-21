85558dbd533210b988a492f63d34db9ccd7a7c2a
gaiadrd <onaircoc@gmail.com>
2026-03-21 17:06:51 +0100

    add: working users/groups/auth api routes + beginning to implement cycle management

--- backend/src/middleware/logger.ts
+++ backend/src/middleware/logger.ts
@@ -1,6 +1,6 @@
 import { NextFunction, RequestHandler } from "express";
 
 export const logger: RequestHandler = (req, res, next) => {
-    console.log(`[LOG TEST] Visited : ${req.method} ${req.url}`);
+    console.log(`[REQ] Visited : ${req.method} ${req.url}`);
     next();
 }
---
+++ backend/src/tasks/newcycle.ts
@@ -0,0 +1,69 @@
+import { PoolConnection } from "mariadb/*";
+import pool from "../dbpool.js";
+import { getAllGroupsData } from "../helpers/groups.js";
+import { GroupData, GroupStatus } from "../types.d.js";
+
+/*
+Le but de cette fonction
+est de choisir l'élu.e de la semaine suivante.
+
+Cette tâche est censée être run par cron
+le dimanche à minuit.
+
+Penser à donner un nom plus explicite. 
+*/
+export async function startNewWeekCycle() {
+    console.log("[POLL] Updating the chosen one for every group.");
+    // on setup la connection à la DB
+    let conn: PoolConnection | undefined;
+    try {
+        conn = await pool.getConnection();
+        await conn.beginTransaction();
+
+        // On récup. la liste de toutes les relations groupes/utilisateurs
+        const groupsUsersRel: Record<string, GroupData> = await getAllGroupsData(conn);
+        
+        // Pour chaque groupe
+        for (const [groupID, data] of Object.entries(groupsUsersRel)) {
+            if (!conn) return;
+
+            // Là on a la liste des utilisateurs dans ce groupe.
+            // Du coup on veut choisir le suivant de celui qui était chosen one avant
+            // (ou un au hasard si le group est tout neuf)
+
+            // Qui était le chosen one avant ?
+            const prevChosenOne: number | null = groupsUsersRel[groupID].chosenOne;
+
+            const usersInGroup = groupsUsersRel[groupID].members;
+
+            let nextChosenOne;
+            if (prevChosenOne) {
+                // On choisit le suivant par roulement
+                const prevChosenOneIndex = usersInGroup.indexOf(prevChosenOne);
+                let nextChosenOneIndex = (prevChosenOneIndex + 1) % (usersInGroup.length);
+                nextChosenOne = usersInGroup[nextChosenOneIndex];
+            } else { // premier élu
+                // full random
+                nextChosenOne = usersInGroup[Math.floor(Math.random() * usersInGroup.length)];
+            }
+
+            console.log(`[POLL] Updating chosen one for group ${groupID} to user ${nextChosenOne}`);
+
+            // mtn on doit faire la MAJ dans la DB
+            const updateResult = await conn.query(
+                `UPDATE Groups
+                SET choosenOneUserId = ?,
+                status = ${GroupStatus.WAITING_FOR_THEME}
+                WHERE id = ?`,
+                [nextChosenOne, groupID]
+            );
+
+            await conn.commit();
+        }
+    } catch (err) {
+        console.log("sql error : " + err);
+        conn?.rollback();
+    } finally {
+        conn?.release();
+    }
+}
