444311560035db54312071737cae4f19fce2568e
Nurulome <bernard.mc98@icloud.com>
2026-03-21 15:50:59 +0100

    sign-in logic and protected routes

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,13 +1,25 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {useAuthStore} from "@/utils/authStore";
 
 export default function RootLayout() {
+    const {isLoggedIn, shouldCreateAccount} = useAuthStore();
+
   return (
-    <>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false}}/>
-      </Stack>
-      <StatusBar style="light" />
-    </>
+    <React.Fragment>
+        <StatusBar style="auto"/>
+        <Stack>
+            <Stack.Protected guard={isLoggedIn}>
+                <Stack.Screen name="(tabs)"/>
+            </Stack.Protected>
+            <Stack.Protected guard={!isLoggedIn}>
+                <Stack.Screen name="sign-in"/>
+                <Stack.Protected guard={shouldCreateAccount}>
+                    <Stack.Screen name="sign-up"/>
+                </Stack.Protected>
+            </Stack.Protected>
+        </Stack>
+    </React.Fragment>
   );
 }
