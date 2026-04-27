409044759c667b3e5a7d94d6007299b7f3d4c673
Mathéo <52327390+Mathuww@users.noreply.github.com>
2026-04-27 18:00:59 +0200

    Preventing double touch

--- frontend/components/UnissonButton.tsx
+++ frontend/components/UnissonButton.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Pressable, StyleSheet, Text } from "react-native";
 
 type Props = {
@@ -7,8 +8,19 @@ type Props = {
 }
 
 export default function Button({label, colorText, OnValidation}: Props) {    
+    const [loading, setLoading] = useState(false);
+
+    const handleTouch = async () => {
+        if (loading) {
+            return;
+        }
+        setLoading(true);
+        await OnValidation();
+        setLoading(false);
+    };
+    
     return (
-        <Pressable style={styles.button} onPress={OnValidation}>
+        <Pressable style={styles.button} onPress={handleTouch}>
             <Text style={[styles.buttonLabel, {color: colorText}]}>{label}</Text>
         </Pressable>
     )
