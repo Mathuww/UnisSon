ea626aa8033571875dc01fb016f048ad5d91c114
Mathéo <52327390+Mathuww@users.noreply.github.com>
2026-04-27 19:13:31 +0200

    Last fixes before MVP

--- backend/src/service/auth.service.ts
+++ backend/src/service/auth.service.ts
@@ -1,9 +1,47 @@
-import { OAuth2Client } from "google-auth-library";
+import { Credentials, OAuth2Client } from "google-auth-library";
 import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
 import { logger } from "../middleware/logger.js";
+import { cli } from "winston/lib/winston/config/index.js";
+import User from "../models/elem/User.model.js";
 
 export class AuthService {
-    static client = new OAuth2Client(process.env.GOOGLE_WEB_CLIENT_ID);
+    static client = new OAuth2Client({
+        client_id: process.env.GOOGLE_WEB_CLIENT_ID, 
+        client_secret: process.env.GOOGLE_WEB_CLIENT_SECRET
+    });
+
+    static async getOAuthClient(user: User): Promise<OAuth2Client> {
+        const client = new OAuth2Client(
+            process.env.GOOGLE_WEB_CLIENT_ID,
+            process.env.GOOGLE_WEB_CLIENT_SECRET
+        );
+
+        client.setCredentials({
+            access_token: user.accessToken,
+            refresh_token: user.refreshToken,
+            expiry_date: user.tokenExpireAt?.getTime()
+        });
+
+        // normalement c'est pas nécéssaire, mais je suis parano
+        client.on('tokens', async (tokens: Credentials) => {
+            logger.info(`updating credientals for google client on user ${user.id}`);
+            await user.update({
+                accessToken: tokens.access_token,
+                tokenExpireAt: tokens.expiry_date ? new Date(tokens.expiry_date) : null
+            });
+        });
+
+        if (!user.tokenExpireAt || user.tokenExpireAt <= new Date()) {
+            const { credentials } = await client.refreshAccessToken();
+            await user.update({
+                accessToken: credentials.access_token,
+                tokenExpireAt: credentials.expiry_date ? new Date(credentials.expiry_date) : null
+            });
+            client.setCredentials(credentials);
+        }
+
+        return client;
+    }
 
     static signToken(payload: JwtPayload) {
         return jwt.sign(
@@ -47,7 +85,6 @@ export class AuthService {
     static async exchangeServerAuthCode(serverAuthCode : string) {
         try {
             return await AuthService.client.getToken(serverAuthCode);
-
         } catch (error) {
             logger.error(error);
         }
