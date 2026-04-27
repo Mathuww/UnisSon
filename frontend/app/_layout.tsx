aa16c90fbb154259b7595878a57dcfda6cc94bac
Mathéo <52327390+Mathuww@users.noreply.github.com>
2026-04-27 13:27:50 +0200

    Yeah, first app in App Store Connect for TestFlight (but, the Google Connect button doesn't work)

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -12,6 +12,7 @@ import {View, StyleSheet} from "react-native";
 GoogleSignin.configure({
     webClientId : '643995195692-0nf56p340fn2us9nendjv0t1v3rvp2kj.apps.googleusercontent.com',
     offlineAccess : true,
+    forceCodeForRefreshToken: true,
     scopes: [
         'https://www.googleapis.com/auth/youtube',
         'https://www.googleapis.com/auth/youtube.force-ssl'
